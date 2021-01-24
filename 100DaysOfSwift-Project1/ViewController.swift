afeb1e408b2737663d2672577d812d7e3527f5fe
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-24 09:31:33 -0300

    Att Viewcontroller para achar as imagens e mostrar no debugger

--- 100DaysOfSwift-Project1/ViewController.swift
+++ 100DaysOfSwift-Project1/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default // Trabalhar com filesystem
+        let path = Bundle.main.resourcePath! // diretorio do bundle do app
+        let items = try! fm.contentsOfDirectory(atPath: path) // conteudo do diretorio
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
 
 
